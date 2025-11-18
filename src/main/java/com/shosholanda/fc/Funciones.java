f7d51534d8fa3aea508eb696817c17d64368e5b4
Rodrigo Ortega <rodrigoortega@ciencias.unam.mx>
2025-11-18 12:45:52 -0600

    feat: funciones.java

--- src/main/java/com/shosholanda/fc/Funciones.java
+++ src/main/java/com/shosholanda/fc/Funciones.java
@@ -10,8 +10,30 @@ public class Funciones {
      * @param n el número a verificar si es primo.
      * @return true si el número es primo, false en otro caso.
      */
-    public static boolean esPrimo(int n){
+    public static boolean esPrimo(int numero){
+        numero = Math.abs(numero);
 
+        if (numero < 2) {
+            return false;
+        }   
+
+        if (numero == 2){
+            return true;
+        }
+
+        if (numero % 2 == 0) {
+            return false;
+        }
+        
+        int limite = (int)Math.sqrt(numero);
+
+        for (int divisor = 3; divisor <= limite; divisor += 2) {
+            if (numero % divisor == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
@@ -21,10 +43,9 @@ public class Funciones {
      * @param mod el módulo de donde sacar el inverso.
      * @return i tal que a + i sea congruente con 0 módulo m.
      */
-    public static int inversoAditivo(int a, int mod){
-
+    public static int inversoAditivo(int valor, int modulo){
+        return modulo(-valor, modulo);
     }
-
 
     /**
      * Nos da el inverso multiplicativo módulo m.
@@ -33,15 +54,47 @@ public class Funciones {
      * @param mod el módulo de donde sacar el inverso.
      * @return i tal que a * i sea congruente con 1 módulo m.
      */
-    public static int inversoMultiplicativo(int a, int mod){
+    public static int inversoMultiplicativo(int valor, int modulo){
+        int moduloPositivo = modulo;
+        int aMod = modulo(valor, moduloPositivo);
 
+        if (aMod == 0){
+           throw new IllegalArgumentException("No existe inverso multiplicativo para 0"); 
+        }
+
+        int coeficiente = 0, nuevoCoeficiente = 1;
+        int residuo = moduloPositivo, nuevoResiduo = aMod;
+
+        while (nuevoResiduo != 0) {
+            int cociente = residuo / nuevoResiduo;
+            int tempCoeficiente = coeficiente - cociente * nuevoCoeficiente;
+            coeficiente = nuevoCoeficiente;
+            nuevoCoeficiente = tempCoeficiente;
+            int tempResiduo = residuo - cociente * nuevoResiduo;
+            residuo = nuevoResiduo;
+            nuevoResiduo = tempResiduo;
+        }
+
+        if (residuo != 1){
+            throw new IllegalArgumentException("No existe inverso multiplicativo para " + valor + " modulo " + modulo);
+        }
+
+        if (coeficiente < 0){
+            coeficiente += moduloPositivo;
+        }
+
+        return coeficiente;
     }
 
     /**
      * Nos da el módulo porque -a%p no funciona como se espera
      */
-    public static int modulo(int n, int mod){
-
+    public static int modulo(int numero, int modulo){
+        int resto = numero % modulo;
+        if (resto < 0){
+            resto += modulo;
+        }
+        return resto;
     }
-    	
+        
 }
